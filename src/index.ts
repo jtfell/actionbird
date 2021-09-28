9387a94b62bf7d4580a274b81acd536916797490
Julian Fell <fell.julian@abc.net.au>
2021-09-28 17:42:59 +1000

    add access token

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { rewriteText } from './text';
 const {
   TWITTER_CONSUMER_KEY,
   TWITTER_CONSUMER_SECRET,
+  TWITTER_ACCESS_TOKEN_KEY,
+  TWITTER_ACCESS_TOKEN_SECRET,
 } = process.env;
 
 //
@@ -48,6 +50,8 @@ const run = async () => {
     subdomain: 'api',
     consumer_key: TWITTER_CONSUMER_KEY,
     consumer_secret: TWITTER_CONSUMER_SECRET,
+    access_token_key: TWITTER_ACCESS_TOKEN_KEY, // from your User (oauth_token)
+    access_token_secret: TWITTER_ACCESS_TOKEN_SECRET, // from your User (oauth_token_secret)
   };
   const client = new Twitter(config);
   const res = await client.getBearerToken();
